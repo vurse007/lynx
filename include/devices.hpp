fc222c9cd0159a494f79b23d2f88c345c522ddbd
NamelessOne <120493345+vurse007@users.noreply.github.com>
2025-05-08 17:50:34 -0700

    chassis constructor and example object

---
+++ include/devices.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "main.h"
+#include "motors.hpp"
+
+namespace global{
+    namespace port{
+        constexpr int FR = 1;
+        constexpr int FL = 2;
+        constexpr int MR = 3;
+        constexpr int ML = 4;
+        constexpr int BR = 5;
+        constexpr int BL = 6;
+
+        constexpr int IMU = 7;
+    }
+
+    lynx::chassis chassis {
+        {
+            {port::FR, pros::E_MOTOR_GEAR_600, false},
+            {port::MR, pros::E_MOTOR_GEAR_600, false},
+            {port::BR, pros::E_MOTOR_GEAR_600, true}
+        },
+        {
+            {port::FL, pros::E_MOTOR_GEAR_600, true},
+            {port::ML, pros::E_MOTOR_GEAR_600, false},
+            {port::BL, pros::E_MOTOR_GEAR_600, true}
+        },
+        2.75,
+        1.0,
+        500
+    };
+
+    pros::Imu imu(port::IMU);
+}
